4d398958911eb720686be576b2571c28ab7e84ae
LAY <achilova_94@list.ru>
2022-12-04 16:52:31 +0300

    Решено д/з-10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая выводит случайное трехзначное число и показывает вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int number = new Random().Next(100, 999);
+Console.WriteLine($"Введите трехзначное число {number}:");
+int numberA = number/100;    
+int numberB = number/10;
+int result = numberB - numberA*10;
+Console.WriteLine($"Получается число {result}"); 
+
+
